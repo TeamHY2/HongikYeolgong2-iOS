9e8ec0edec6bc6af1c51f4fa0fd4ce6e2375f78a
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-09-20 04:22:45 +0900

    Initial Commit

---
+++ HongikYeolgong2/HongikYeolgong2App.swift
@@ -0,0 +1,17 @@
+//
+//  HongikYeolgong2App.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HongikYeolgong2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
