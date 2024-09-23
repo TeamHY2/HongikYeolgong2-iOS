87d616af9743ec65a28299e4c2b1886e9e9214da
Seokki-Kwon <seokki0706@gmail.com>
2024-09-23 23:50:04 +0900

    feat: TabView 추가

---
+++ HongikYeolgong2/Presentation/Ranking/RankingView.swift
@@ -0,0 +1,18 @@
+//
+//  RankingView.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 9/23/24.
+//
+
+import SwiftUI
+
+struct RankingView: View {
+    var body: some View {
+        Text("RankingView")
+    }
+}
+
+#Preview {
+    RankingView()
+}
