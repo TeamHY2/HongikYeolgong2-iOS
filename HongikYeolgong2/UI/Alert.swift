982d2ce86895c1e182acaec9f59b1740bbca05c2
Seokki-Kwon <seokki0706@gmail.com>
2024-09-24 01:33:48 +0900

    feat: 공통 컴포넌트 추가(Button, Alert, TextField)

---
+++ HongikYeolgong2/UI/Alert.swift
@@ -0,0 +1,127 @@
+//
+//  Alert.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 9/24/24.
+//
+
+import SwiftUI
+import Combine
+
+struct AlertModifier: ViewModifier {
+    
+    var confirmButtonText: String = "네"
+    var cancleButtonText: String = "아니오"
+    
+    let title: String
+    let confirmAction: (() -> ())?
+    let cancleAction: (() -> ())?
+    
+    @State private var scale: CGFloat = 1
+    
+    @Binding var isPresented: Bool
+    
+    init(title: String, confirmButtonText: String, cancleButtonText: String, confirmAction: (() -> Void)?, cancleAction: (() -> Void)? = nil, isPresented: Binding<Bool>) {
+        self.confirmButtonText = confirmButtonText
+        self.cancleButtonText = cancleButtonText
+        self.title = title
+        self.confirmAction = confirmAction
+        self.cancleAction = cancleAction
+        self._isPresented = isPresented
+    }
+    
+    init(title: String, confirmAction: (() -> Void)?, cancleAction: (() -> Void)? = nil, isPresented: Binding<Bool>) {
+        self.title = title
+        self.confirmAction = confirmAction
+        self.cancleAction = cancleAction
+        self._isPresented = isPresented
+    }
+    
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay {
+                if isPresented {
+                    ZStack {
+                        Color.black
+                            .opacity(0.5)
+                            .ignoresSafeArea()
+                        
+                        VStack(spacing: 0) {
+                            Spacer().frame(height: 40)
+                            
+                            // title
+                            Text(title)
+                                .font(.pretendard(size: 18, weight: .semibold))
+                                .foregroundStyle(.gray100)
+                            
+                            Spacer().frame(height: 30)
+                            
+                            HStack {
+                                Button(action: {
+                                    cancleAction?()
+                                    isPresented = false
+                                }) {
+                                    Text(cancleButtonText)
+                                        .font(.pretendard(size: 16, weight: .semibold))
+                                        .foregroundStyle(.gray200)
+                                        .frame(maxWidth: .infinity, minHeight: 46)
+                                }
+                                .background(.gray600)
+                                .cornerRadius(8)
+                                
+                                Spacer().frame(width: 12)
+                                
+                                Button(action: {
+                                    confirmAction?()
+                                    isPresented = false
+                                }) {
+                                    Text(confirmButtonText)
+                                        .font(.pretendard(size: 16, weight: .semibold))
+                                        .foregroundStyle(.white)
+                                        .frame(maxWidth: .infinity, minHeight: 46)
+                                }
+                                .background(.blue100)
+                                .cornerRadius(8)
+                                
+                            }
+                            .padding(.horizontal, 24)
+                            
+                            Spacer().frame(height: 30)
+                        }
+                        .frame(maxWidth: 316)
+                        .background(.gray800)
+                        .cornerRadius(8)
+                        .scaleEffect(scale)
+                        .animation(.interpolatingSpring(mass: 1.0, stiffness: 200, damping: 15), value: scale)
+                        }
+                        
+                        .onReceive(Just(isPresented), perform: { newValue in
+                            if newValue {
+                                scale = 1.0
+                            } else {
+                                scale = 0.9
+                            }
+                        })
+                }
+            }
+    }
+}
+
+extension View {
+    func alert(title: String, isPresented: Binding<Bool>, confirmAction: (() -> Void)?, cancelAction: (() -> Void)?) -> some View {
+        modifier(AlertModifier(title: title, confirmAction: confirmAction, cancleAction: cancelAction, isPresented: isPresented))
+    }
+    
+    func alert(title: String, isPresented: Binding<Bool>, confirmAction: (() -> Void)?) -> some View {
+        modifier(AlertModifier(title: title, confirmAction: confirmAction, isPresented: isPresented))
+    }
+    
+    func alert(title: String, confirmButtonText: String, cancleButtonText: String, isPresented: Binding<Bool>, confirmAction: (() -> Void)?, cancelAction: (() -> Void)?) -> some View {
+        modifier(AlertModifier(title: title, confirmButtonText: confirmButtonText, cancleButtonText: cancleButtonText, confirmAction: confirmAction, cancleAction: cancelAction, isPresented: isPresented))
+    }
+    
+    func alert(title: String, confirmButtonText: String, cancleButtonText: String, isPresented: Binding<Bool>, confirmAction: (() -> Void)?) -> some View {
+        modifier(AlertModifier(title: title, confirmButtonText: confirmButtonText, cancleButtonText: cancleButtonText, confirmAction: confirmAction, isPresented: isPresented))
+    }
+}
---
+++ HongikYeolgong2/UI/HY2TextField.swift
@@ -0,0 +1,50 @@
+//
+//  HY2TextField.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 9/24/24.
+//
+
+import SwiftUI
+
+struct HY2TextField: View {
+    @Binding var text: String
+    @FocusState var isFocused: Bool
+    
+    let placeholder: String
+    var isError: Bool
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .frame(maxWidth: .infinity, maxHeight: 48)
+                .foregroundColor(.gray800)
+                .cornerRadius(8)
+            
+            HStack {
+                TextField(text: $text) {
+                    Text(placeholder)
+                        .font(.pretendard(size: 16, weight: .regular))
+                        .foregroundStyle(.gray400)
+                }
+                .focused($isFocused)
+                .foregroundColor(.gray200)
+                .padding(.leading, 16)
+                .padding(.trailing, 8)
+                
+                if !text.isEmpty {
+                    Image(.closeButton)
+                        .padding(.trailing, 14)
+                        .onTapGesture {
+                            text = ""
+                        }
+                }
+            }
+        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(isError && !text.isEmpty ? .yellow100 : .gray400)
+                .opacity(isFocused ? 1 : 0)
+        )
+    }
+}
