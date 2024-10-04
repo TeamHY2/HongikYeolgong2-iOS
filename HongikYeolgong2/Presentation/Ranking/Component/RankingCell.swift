5b447216059e0a71ef1eb26f10a91a74a3b8b497
Seokki-Kwon <seokki0706@gmail.com>
2024-10-04 18:37:03 +0900

    feat: lineHeight 적용을위한 extension 추가

---
+++ HongikYeolgong2/Presentation/Ranking/Component/RankingCell.swift
@@ -0,0 +1,47 @@
+//
+//  RankingCell.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 10/4/24.
+//
+
+import SwiftUI
+
+struct RankingCell: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(spacing: 0) {
+                HStack(spacing: 14) {
+                    Text("1")
+                        .font(.pretendard(size: 16, weight: .regular))
+                        .foregroundStyle(.gray100)
+                    
+                    Text("건축학부")
+                        .fontWithLineHeight(.pretendard(size: 16, weight: .regular), lineHeight: 26)
+                        .foregroundStyle(.gray100)
+                }
+                
+                Spacer()
+                
+                HStack(spacing: 22) {
+                    Text("120H")
+                        .font(.pretendard(size: 12, weight: .regular))
+                        .foregroundStyle(.gray100)
+                    HStack(spacing: 7) {
+                        Text("+1")
+                            .font(.pretendard(size: 16, weight: .regular))
+                            .foregroundStyle(.gray100)
+                        Image(.polygonGray)
+                    }
+                }
+            }
+            .padding(EdgeInsets(top: 13, leading: 24, bottom: 13, trailing: 24))
+        }
+        .background(.gray800)
+        .cornerRadius(8)        
+    }
+}
+
+#Preview {
+    RankingCell()
+}
