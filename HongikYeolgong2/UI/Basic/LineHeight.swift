082301651043f2b0fee48b48dc95f7f83538c706
Seokki-Kwon <seokki0706@gmail.com>
2024-10-04 18:37:03 +0900

    feat: lineHeight 적용을위한 extension 추가

---
+++ HongikYeolgong2/UI/Basic/LineHeight.swift
@@ -0,0 +1,25 @@
+//
+//  LineHeight.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 10/4/24.
+//
+
+import SwiftUI
+import UIKit
+
+extension View {
+    // lineHeight - fontHeight = spacing
+    // lineHeight - fontHeight / 2
+    
+    // UIFont로 높이를 구하고 다시 Font로 변경
+    // UIFont -> Font는 가능 그 반대는 불가능
+    func fontWithLineHeight(_ font: UIFont, lineHeight: CGFloat) -> some View {
+        let fontHeight = font.lineHeight
+        
+        return self
+            .font(Font(font))
+            .lineSpacing(lineHeight - font.lineHeight)
+            .padding(.vertical, (lineHeight - fontHeight) / 2)
+    }
+}
