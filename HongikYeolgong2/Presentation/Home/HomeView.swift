9b0a4167087cb8eae1921a6ddf752fd39e546ddc
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-10-05 19:15:23 +0900

    fix: 홈화면 백그라운드 잘림현상 수정

--- HongikYeolgong2/Presentation/Home/HomeView.swift
+++ HongikYeolgong2/Presentation/Home/HomeView.swift
@@ -13,28 +13,33 @@ struct HomeView: View {
             WeeklyStudy()
             
             TodayWiseSaying()
-                .padding(.top, 120)
+                .padding(.top, 120.adjustToScreenHeight)
             
             Spacer()
             Spacer()
             HStack(spacing: 12.adjustToScreenWidth) {
                 Button(action: {}) {
                     Text("좌석")
-                        .frame(maxWidth: 69, minHeight: 52.adjustToScreenHeight)
+                        .frame(width: 69.adjustToScreenWidth, height: 52.adjustToScreenHeight)
                         .font(.suite(size: 16, weight: .semibold))
                         .foregroundStyle(.white)
                 }
-                .background(Image(.seatButton).resizable().frame(maxWidth: .infinity, minHeight: 52.adjustToScreenHeight))
+                .background(Image(.seatButton)
+                    .resizable()
+                    .frame(maxWidth: .infinity,
+                           minHeight: 52.adjustToScreenHeight))
                 
                 Button(action: {}) {
                     Text("열람실 이용 시작")
-                        .frame(maxWidth: .infinity, 
+                        .frame(maxWidth: .infinity,
                                minHeight: 52.adjustToScreenHeight)
                         .font(.suite(size: 16, weight: .semibold))
                         .foregroundStyle(.white)
                 }
-                .background(Image(.startButton).resizable().frame(maxWidth: .infinity, 
-                                                                  minHeight: 52.adjustToScreenHeight))
+                .background(Image(.startButton)
+                    .resizable()
+                    .frame(maxWidth: .infinity,
+                           minHeight: 52.adjustToScreenHeight))
             }
             
             Spacer()
@@ -43,8 +48,8 @@ struct HomeView: View {
         .background(
             Image(.iOSBackground)
                 .resizable()
-                .frame(maxWidth: .infinity)
                 .ignoresSafeArea(.all)
+                .frame(maxWidth: .infinity, minHeight: SafeAreaHelper.getFullScreenHeight())
                 .allowsHitTesting(false)
         )
     }
