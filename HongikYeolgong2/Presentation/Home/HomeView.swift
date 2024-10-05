ca7979bbe677d7b6cc159728cd06f942a3e0025d
Seokki-Kwon <seokki0706@gmail.com>
2024-10-05 16:48:10 +0900

    feat: 홈뷰 퍼블리싱

--- HongikYeolgong2/Presentation/Home/HomeView.swift
+++ HongikYeolgong2/Presentation/Home/HomeView.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-            Text("HomeView")
+        VStack {
+                WeeklyStudy()
+            
+            TodayWiseSaying()
+                .padding(.top, 120)
+            
+            Spacer()
+            Spacer()
+            HStack(spacing: 12) {
+                Button(action: {}) {
+                    Text("좌석")
+                        .frame(maxWidth: 69, minHeight: 52)
+                        .font(.suite(size: 16, weight: .semibold))
+                        .foregroundStyle(.white)
+                }
+                .background(Image(.seatButton))
+                
+                Button(action: {}) {
+                    Text("열람실 이용 시작")
+                        .frame(maxWidth: .infinity, minHeight: 52)
+                        .font(.suite(size: 16, weight: .semibold))
+                        .foregroundStyle(.white)
+                }
+                .background(Image(.startButton))
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal, 32)
+        .background(
+            Image(.iOSBackground)
+                .ignoresSafeArea(.all)
+                .allowsHitTesting(false)
+        )
     }
 }
 
