39a5a75e418df72df375c04729e36fb40798a0ab
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-10-05 18:34:42 +0900

    fix: SafeArea 유무에 따라서 TabBar 사이즈 조정

---
+++ HongikYeolgong2/UI/Basic/SafeArea.swift
@@ -0,0 +1,22 @@
+//
+//  SafeArea.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 10/5/24.
+//
+
+import UIKit
+
+final class SafeAreaHelper {
+    private init() {}
+    
+    static func safeAreaBottomInset() -> CGFloat {
+        if #available(iOS 11.0, *) {
+            let window = UIApplication.shared.keyWindow
+            let bottomPadding = window?.safeAreaInsets.bottom
+            return bottomPadding ??  0.0
+        } else {
+            return 0.0
+        }
+    }
+}
