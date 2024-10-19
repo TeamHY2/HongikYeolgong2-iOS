a270f510433bb615cd30c3f3d07feb6698f4b1f5
Seokki-Kwon <seokki0706@gmail.com>
2024-10-19 23:03:34 +0900

    refactor: 학과리스트 Enum으로 변경

--- HongikYeolgong2/Presentation/Auth/SignInView.swift
+++ HongikYeolgong2/Presentation/Auth/SignInView.swift
@@ -10,50 +10,9 @@ import SwiftUI
 struct SignInView: View {
     @Environment(\.injected) var injected: DIContainer
     @State private var isDisabled = true
-    @State private var selectedDepartment = ""
+    @State private var selectedDepartment: Department = .appliedArts
     @State private var inputDepartment = ""
     @State private var nickname = ""
-    
-    private let departments = [
-        "건설도시공학부",
-        "건설환경공학과",
-        "건축학부",
-        "경영학부",
-        "경제학부",
-        "공연예술학부",
-        "금속조형디자인과",
-        "기계시스템디자인공학과",
-        "국어교육과",
-        "국어국문학과",
-        "도시공학과",
-        "독어독문학과",
-        "동양화과",
-        "도예유리과",
-        "디자인경영융합학부",
-        "디자인·예술경영학부",
-        "디자인학부",
-        "물리교육과",
-        "법학부",
-        "불어불문학과",
-        "사회교육과",
-        "산업디자인학과",
-        "산업·데이터공학과",
-        "섬유미술패션디자인과",
-        "수학교육과",
-        "신소재화공시스템공학부",
-        "영어교육과",
-        "영어영문학과",
-        "역사교육과",
-        "예술학과",
-        "응용미술학과",
-        "일본어문학과",
-        "전자전기공학부",
-        "조소과",
-        "컴퓨터공학과",
-        "판화과",
-        "프랑스어문학과",
-        "회화과"
-    ]
     
     var body: some View {
         ZStack {
@@ -134,9 +93,13 @@ struct SignInView: View {
             }
             
             DropDownPicker(text: $inputDepartment,
-                           seletedItem: $selectedDepartment,
+                           seletedItem: Binding(get: {
+                selectedDepartment.rawValue
+            }, set: {
+                selectedDepartment = .init(rawValue: $0) ?? .appliedArts
+            }),
                            placeholder: "학과를 입력해주세요",
-                           items: departments)
+                           items: Department.allCases.map { $0.rawValue })
             .padding(.top, 8)
         }
         .padding(.top, 12)
