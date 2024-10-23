070bb3209a6eb0273f76b9cf36a522605fbd3190
Seokki-Kwon <seokki0706@gmail.com>
2024-10-23 17:37:59 +0900

    refactor: UserDataInteractor에서 애플로그인 요청하도록 변경

--- HongikYeolgong2/Core/AppEnviroment.swift
+++ HongikYeolgong2/Core/AppEnviroment.swift
@@ -15,8 +15,9 @@ extension AppEnviroment {
     static func bootstrap() -> AppEnviroment {
         let appState = Store<AppState>(AppState())
         let authRepository = AuthRepositoryImpl()
-        let interactors: DIContainer.Interactors = .init(userDataInteractor: UserDataInteractorImpl(appState: appState, authRepository: authRepository))
         let services: DIContainer.Services = .init(authenticationService: AuthenticationServiceImpl())
+        let interactors: DIContainer.Interactors = .init(userDataInteractor: UserDataInteractorImpl(appState: appState, authRepository: authRepository, authenticationService: services.authenticationService))
+        
         let diContainer = DIContainer(appState: appState, interactors: interactors, services: services)
         return .init(container: diContainer)
     }
