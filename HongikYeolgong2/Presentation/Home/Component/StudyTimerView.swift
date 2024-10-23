bd102e30ca44db7db6bc5d65fb6a1d1f5d402bf5
Seokki-Kwon <seokki0706@gmail.com>
2024-10-23 23:02:47 +0900

    feat: 공부시간을 나타내는 StudyTimerView 추가

---
+++ HongikYeolgong2/Presentation/Home/Component/StudyTimerView.swift
@@ -0,0 +1,48 @@
+//
+//  StudyTimerView.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 10/23/24.
+//
+
+import SwiftUI
+
+struct StudyTimerView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("Time Left")
+                .font(.suite(size: 12, weight: .medium), lineHeight: 15.adjustToScreenHeight)
+                .foregroundStyle(.gray300)
+            
+            Text("03:52:00")
+                .font(.suite(size: 30, weight: .black), lineHeight: 32.adjustToScreenHeight)
+                .foregroundColor(.gray100)
+                .padding(.top, 11)
+            
+            LinearProgressView(shape: Rectangle(), value: 0.3)
+                .frame(height: 8)
+                .padding(.top, 16)
+        }
+    }
+}
+
+struct LinearProgressView<Shape: SwiftUI.Shape>: View {
+    var shape: Shape
+    let value: Double
+    
+    var body: some View {
+        VStack {
+            shape.fill(.gray600)
+                .overlay(alignment: .leading) {
+                    GeometryReader { proxy in
+                        shape.fill(.blue100)
+                            .frame(width: proxy.size.width * value)
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    StudyTimerView()
+}
