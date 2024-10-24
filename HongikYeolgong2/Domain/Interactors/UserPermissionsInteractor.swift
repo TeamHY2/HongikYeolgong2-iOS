6068b3c287fa8364a74e6cbc1f3237ab509b5b74
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-10-24 16:31:45 +0900

    docs: 주요코드 문서화

--- HongikYeolgong2/Domain/Interactors/UserPermissionsInteractor.swift
+++ HongikYeolgong2/Domain/Interactors/UserPermissionsInteractor.swift
@@ -14,9 +14,16 @@ enum Permission {
 
 extension Permission {
     enum Status: Equatable {
+        /// 아직 확인되지 않은 상태
         case unknown
+        
+        /// 사용자에게 권한을 요청하기 전 상태
         case notRequested
+        
+        /// 사용자가 권한을 승인한 상태
         case granted
+        
+        ///  사용자가 권한을 거부함 -> 앱설정 이동
         case denied
     }
 }
@@ -35,6 +42,8 @@ final class RealUserPermissionsInteractor: UserPermissionsInteractor {
         self.openAppSetting = openAppSetting
     }
     
+    ///  특정 권한 상태를 확인하고 앱 상태를 업데이트 합니다.
+    /// - Parameter permission: 확인할 권한 종류
     func resolveStatus(for permission: Permission) {
         let keyPath = AppState.permissionKeyPath(for: .localNotifications)
         let currentStatus = appState[keyPath]
@@ -48,6 +57,8 @@ final class RealUserPermissionsInteractor: UserPermissionsInteractor {
         }
     }
     
+    /// 유저에게 권한을 요청합니다.
+    /// - Parameter permission: 확인할 권한 종류
     func request(permission: Permission) {
         let keyPath = AppState.permissionKeyPath(for: .localNotifications)
         let currentStatus = appState[keyPath]
@@ -74,6 +85,8 @@ extension UNAuthorizationStatus {
 }
 
 private extension RealUserPermissionsInteractor {
+    /// 실제 LocalNotification 권한의 상태를 체크하고 앱상태를 업데이트
+    /// - Parameter resolve: 권한확인 결과를 반환받을 클로저
     func localNotificationsPermissionStatus(_ resolve: @escaping (Permission.Status) -> Void) {
         let center = UNUserNotificationCenter.current()
         center.getNotificationSettings { settings in
@@ -83,6 +96,7 @@ private extension RealUserPermissionsInteractor {
         }
     }
     
+    /// LocalNotification 권한을 요청하고 앱상태를 업데이트
     func requestLocalNotificationsPermission() {
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .sound]) { (isGranted, error) in
