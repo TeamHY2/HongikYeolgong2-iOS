102b179c10ef218ed298f0cd62f8e937acb50daf
Seokki-Kwon <seokki0706@gmail.com>
2024-10-25 02:57:43 +0900

    docs: 주요메서드 문서화 & getWeekly -> getWeeklyStduyRecords 네이밍 변경

--- HongikYeolgong2/Domain/Interactors/StudySessionInteractor.swift
+++ HongikYeolgong2/Domain/Interactors/StudySessionInteractor.swift
@@ -18,10 +18,12 @@ final class StudySessionInteractorImpl: StudySessionInteractor {
     init(studySessionRepository: StudySessionRepository) {
         self.studySessionRepository = studySessionRepository
     }
-    
+        
+    /// 한 주에 대한 공부 횟수를 가져옵니다.
+    /// - Parameter studyRecords: 공부 기록(월 - 일)
     func getWeekyStudy(studyRecords: Binding<[WeeklyStudyRecord]>) {
         studySessionRepository
-            .getWeelyStudy()
+            .getWeeklyStudyRecords()
             .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: { _ in }) {
                 studyRecords.wrappedValue = $0.map { $0.toEntity() }
