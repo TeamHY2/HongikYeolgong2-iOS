dbf5d18eee6925aa444c402c41b86d1376a8666b
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-10-31 11:50:05 +0900

    feat: WeeklyRepository 추가

---
+++ HongikYeolgong2/Data/Repositories/Weekly/WeeklyRepositoryImpl.swift
@@ -0,0 +1,14 @@
+//
+//  WeeklyRepositoryImpl.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 10/31/24.
+//
+
+import Foundation
+
+final class WeeklyRepositoryImpl: WeeklyRepository {
+    func getWeekField() {
+        
+    }
+}
