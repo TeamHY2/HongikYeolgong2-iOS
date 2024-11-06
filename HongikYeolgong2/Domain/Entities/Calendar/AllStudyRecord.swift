e79351e5634f90a09d573635e3212fbf35126f12
owllwo98 <sloopy98@naver.com>
2024-11-07 04:39:45 +0900

    *refactor: 불필요한 부분 삭제

--- HongikYeolgong2/Domain/Entities/Calendar/AllStudyRecord.swift
+++ HongikYeolgong2/Domain/Entities/Calendar/AllStudyRecord.swift
@@ -7,19 +7,6 @@ struct AllStudyRecord {
     let year: String
     let date: Date
     let studyCount: Int
-    
-    var imageName: String {
-        switch studyCount {
-        case 0:
-            "day_Count00"
-        case 1:
-            "day_Count01"
-        case 2:
-            "day_Count02"
-        default:
-            "day_Count03"
-        }
-    }
 }
 
 
