74eb6123f5331d7e95a19de586469f5ad05fbe3d
Seokki-Kwon <seokki0706@gmail.com>
2024-11-08 02:43:29 +0900

    feat: 열람실 이용종료 30분, 10분전 Notifiaction 처리

---
+++ HongikYeolgong2/Models/StudyNotificationType.swift
@@ -0,0 +1,31 @@
+//
+//  StudyNotificationType.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 11/8/24.
+//
+
+import Foundation
+
+enum StudyNotificationType {
+    case extensionAvailable
+    case urgent
+    
+    var timeOffset: TimeInterval {
+        switch self {
+        case .extensionAvailable:
+            return .init(seconds: 30)
+        case .urgent:
+            return  .init(seconds: 10)
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .extensionAvailable:
+            return "열람실 시간 종료 30분 전이에요. 지금부터 열람실 연장이 가능해요!"
+        case .urgent:
+            return "열람실 시간 종료 10분 전이에요. 열람실 연장이 필요하다면 서둘러주세요!"
+        }
+    }
+}
