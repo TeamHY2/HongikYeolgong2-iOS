842f7677e7362b137aac2c72f5d226de4712b1b9
owllwo98 <sloopy98@naver.com>
2024-11-08 17:47:08 +0900

    *feat: 화면 데이터 로딩 해결

--- HongikYeolgong2/Presentation/Record/RecordView.swift
+++ HongikYeolgong2/Presentation/Record/RecordView.swift
@@ -13,33 +13,35 @@ struct RecordView: View {
     @State var studyTime = StudyTime()
     
     var body: some View {
-        CaledarView()
-        VStack(spacing: 13.adjustToScreenHeight) {
-            
-            Spacer()
-            // 기록 정보 출력부분
-            HStack(spacing: 13.adjustToScreenWidth) {
-                RecordCell(title: "연간",
-                           hours: studyTime.yearHours,
-                           minutes: studyTime.yearMinutes)
-                RecordCell(title: "이번학기",
-                           hours: studyTime.semesterHours,
-                           minutes: studyTime.semesterMinutes)
+        VStack {
+            CaledarView()
+            VStack(spacing: 13.adjustToScreenHeight) {
+                
+                Spacer()
+                // 기록 정보 출력부분
+                HStack(spacing: 13.adjustToScreenWidth) {
+                    RecordCell(title: "연간",
+                               hours: studyTime.yearHours,
+                               minutes: studyTime.yearMinutes)
+                    RecordCell(title: "이번학기",
+                               hours: studyTime.semesterHours,
+                               minutes: studyTime.semesterMinutes)
+                }
+                HStack(spacing: 13.adjustToScreenWidth) {
+                    RecordCell(title: "월간",
+                               hours: studyTime.monthHours,
+                               minutes: studyTime.monthMinutes)
+                    RecordCell(title: "투데이",
+                               hours: studyTime.dayHours,
+                               minutes: studyTime.dayMinutes)
+                }
             }
-            HStack(spacing: 13.adjustToScreenWidth) {
-                RecordCell(title: "월간",
-                           hours: studyTime.monthHours,
-                           minutes: studyTime.monthMinutes)
-                RecordCell(title: "투데이",
-                           hours: studyTime.dayHours,
-                           minutes: studyTime.dayMinutes)
+            .padding(.horizontal, 32.adjustToScreenWidth)
+            .padding(.bottom, 36.adjustToScreenHeight)
+            .onAppear {
+                // 이용 시간 가져오기
+                studyTimeInteractor.getStudyTime(StudyTime: $studyTime)
             }
-        }
-        .padding(.horizontal, 32.adjustToScreenWidth)
-        .padding(.bottom, 36.adjustToScreenHeight)
-        .onAppear {
-            // 이용 시간 가져오기
-            studyTimeInteractor.getStudyTime(StudyTime: $studyTime)
         }
     }
 }
