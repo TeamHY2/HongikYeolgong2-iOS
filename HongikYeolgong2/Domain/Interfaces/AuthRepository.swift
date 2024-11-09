ab845d4b3267f40b94a1464d5cb3142cecf1b2d5
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-11-09 18:43:06 +0900

    feat: 회원탈퇴 API 연동 및 관련 DTO 추가

--- HongikYeolgong2/Domain/Interfaces/AuthRepository.swift
+++ HongikYeolgong2/Domain/Interfaces/AuthRepository.swift
@@ -13,4 +13,5 @@ protocol AuthRepository {
     func checkUserNickname(nickname: String) -> AnyPublisher<Bool, NetworkError>
     func getUser() -> AnyPublisher<SignUpResponseDTO, NetworkError>
     func validToken() -> AnyPublisher<TokenValidResponseDTO, NetworkError>
+    func withdraw() -> AnyPublisher<Void, NetworkError>
 }
