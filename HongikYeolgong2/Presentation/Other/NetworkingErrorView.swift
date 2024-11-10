cc836584d360d2bc8d753b463dc656941e8bf787
Choe-ju <juwon3415@gmail.com>
2024-11-10 15:36:37 +0900

    feat: NettworkingErrorView 구현 (임시)

---
+++ HongikYeolgong2/Presentation/Other/NetworkingErrorView.swift
@@ -0,0 +1,63 @@
+//
+//  NetworkingErrorView.swift
+//  HongikYeolgong2
+//
+//  Created by 최주원 on 11/9/24.
+//
+
+import SwiftUI
+
+struct NetworkingErrorView: View {
+    //@Binding var showAlert: Bool
+    
+    var body: some View {
+        VStack(spacing: 16.adjustToScreenHeight) {
+            Image(systemName: "exclamationmark.octagon.fill")
+                .font(.system(size: 48))
+                .foregroundColor(.blue100)
+            
+            Text("네트워크가 통신이 원할하지 않습니다.\n확인 후 다시 시도해주시기 바랍니다")
+                .multilineTextAlignment(.center)
+                .foregroundColor(.gray100)
+                .font(.body)
+            
+            Spacer()
+                .frame(height: 0)
+            
+            HStack(spacing: 12.adjustToScreenWidth) {
+                Button(action: {
+                    //showAlert = false
+                }) {
+                    Text("종료하기")
+                        .foregroundColor(.gray200)
+                        .padding(.vertical, 10)
+                        .frame(maxWidth: .infinity)
+                        .background(.gray600)
+                        .cornerRadius(8)
+                }
+                
+                Button(action: {
+                    //showAlert = false
+                }) {
+                    Text("다시시도")
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .frame(maxWidth: .infinity)
+                        .background(.blue100)
+                        .cornerRadius(8)
+                }
+            }
+            .padding(.horizontal, 24)
+        }
+        .padding()
+        .background(.gray800)
+        .cornerRadius(16)
+        .shadow(radius: 10)
+        .padding(.horizontal, 40)
+    }
+}
+
+#Preview {
+    NetworkingErrorView()
+}
