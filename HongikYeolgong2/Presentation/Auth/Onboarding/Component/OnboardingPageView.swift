85bd93cb1a9fe9d9daefe540a08b8423b229a44e
Seokki-Kwon <seokki0706@gmail.com>
2024-11-12 03:13:37 +0900

    refactor: OnboardingView 컴포넌트별로 분리

---
+++ HongikYeolgong2/Presentation/Auth/Onboarding/Component/OnboardingPageView.swift
@@ -0,0 +1,45 @@
+//
+//  OnboardingPageView.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 11/12/24.
+//
+
+import SwiftUI
+
+struct OnboardingPageView: View {
+    @Binding var tabIndex: Int
+    
+    var body: some View {
+        VStack {
+            TabView(selection: $tabIndex) {
+                Image(.onboarding01)
+                    .tag(0)
+                Image(.onboarding02)
+                    .tag(1)
+                Image(.onboarding03)
+                    .tag(2)
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            
+            HStack(spacing: 16.adjustToScreenWidth) {
+                ForEach(0..<3, id: \.self) { index in
+                    Group {
+                        if index == tabIndex {
+                            Image(.shineCount02)
+                                .frame(width: 9, height: 9)
+                        } else {
+                            Circle()
+                                .frame(width: 9, height: 9)
+                                .foregroundColor(.gray600)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    OnboardingPageView(tabIndex: .constant(0))
+}
