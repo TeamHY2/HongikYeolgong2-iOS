95c5fccf1e394d21e86001d0bf406c9c30e796c7
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-11-25 16:59:57 +0900

    feat: AppVersion 체크 기능 추가

---
+++ HongikYeolgong2/Util/Services/RemoteConfigManager.swift
@@ -0,0 +1,32 @@
+//
+//  RemoteConfigManager.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 11/25/24.
+//
+
+import Foundation
+
+import FirebaseRemoteConfig
+
+struct RemoteConfigManager {
+    static let shared = RemoteConfigManager()
+    
+    let remoteConfig = RemoteConfig.remoteConfig()
+    let settings = RemoteConfigSettings()
+    
+    init() {
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+    }
+    
+    func getMinimumAppVersion() async -> Int? {
+        do {
+            try await remoteConfig.fetch()
+            try await remoteConfig.activate()
+            return remoteConfig["appVersion"].numberValue.intValue
+        } catch {
+            return nil
+        }
+    }
+}
