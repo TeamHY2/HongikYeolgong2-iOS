e10decb51b5a4ea9d4fcacb7c0a99186efa80021
Choe-ju <juwon3415@gmail.com>
2024-12-05 00:03:55 +0900

    fix: PromotionPopupView 표현 방식 수정
    - Promotion관련 데이터 로딩 완료 후  .authenticated 상태로 변경 될 수 있도록 구현
      - checkPromotionPresent() 순차적으로 진행
      - PromotionPopupView를 이미지 파일을 받아온 후 완료 처리
    - 상세보기 WebView 실행 시 PromotionPopupView 닫히도록 수정

---
+++ HongikYeolgong2/Data/DTO/Promotion/PromotionResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  PromotionResponseDTO.swift
+//  HongikYeolgong2
+//
+//  Created by 최주원 on 12/4/24.
+//
+
+import SwiftUI
+
+struct PromotionResponseDTO: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PromotionResponseDTO()
+}
