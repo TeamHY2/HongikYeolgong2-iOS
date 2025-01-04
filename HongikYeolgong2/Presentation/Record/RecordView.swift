ad73aac68708bcb598d24bc20f7fd9f19ea7db12
Choe-ju <juwon3415@gmail.com>
2025-01-04 18:16:42 +0900

    feat: 캘린더뷰 UI 구성

--- HongikYeolgong2/Presentation/Record/RecordView.swift
+++ HongikYeolgong2/Presentation/Record/RecordView.swift
@@ -6,17 +6,10 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct RecordView: View {
-    
-    private var records: [(title: String, hours: Int, minutes: Int)] {
-        [
-            ("연간", 10, 2),
-            ("이번학기", 3, 2),
-            ("월간", 3, 2),
-            ("투데이", 1, 5)
-        ]
-    }
+    let store: StoreOf<RecordFeature>
     
     var body: some View {
         VStack(spacing: 13.adjustToScreenHeight) {
@@ -31,52 +24,70 @@ struct RecordView: View {
     }
     
     var calendar: some View {
-        Text("calendar")
+        VStack {
+            // Header
+            HStack {
+                Text(store.selectedDate.getMonthString())
+                    .font(.suite(size: 24, weight: .bold))
+                
+                Text(store.selectedDate.getYearString())
+                    .font(.suite(size: 24, weight: .bold))
+                
+                Spacer()
+                
+                HStack {
+                    Button {
+                        store.send(.changeMonth(.prev))
+                    } label: {
+                        Image(.icCalendarLeft)
+                    }
+                    
+                    Button {
+                        store.send(.changeMonth(.next))
+                    } label: {
+                        Image(.icCalendarRight)
+                    }
+                }
+            }
+            .padding()
+            
+            // Weekdays
+            HStack {
+                ForEach(WeekDayEng.allCases, id: \.self) { day in
+                    Text(day.rawValue)
+                        .font(.suite(size: 12, weight: .medium))
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            
+            // Days Grid
+            LazyVGrid(columns: Array(repeating: .init(.flexible(), spacing: 5), count: 7), spacing: 5) {
+                ForEach(store.currentMonth, id: \.id) { day in
+                    CalendarCell(dayInfo: day)
+                }
+            }
+        }
+        .onAppear {
+            store.send(.onAppear)
+        }
     }
     
     /// 하단 기록 뷰
     var record: some View {
         VStack(spacing: 13.adjustToScreenHeight) {
-            ForEach(0..<records.count / 2, id: \.self) { index in
+            ForEach(0..<store.records.count / 2, id: \.self) { index in
                 HStack(spacing: 13.adjustToScreenWidth) {
-                    RecordCell(record: records[index * 2])
-                    RecordCell(record: records[index * 2 + 1])
+                    RecordCell(record: store.records[index * 2])
+                    RecordCell(record: store.records[index * 2 + 1])
                 }
             }
         }
     }
 }
 
-struct RecordCell: View {
-    let record: (title: String, hours: Int, minutes: Int)
-    
-    var body: some View {
-        HStack{
-            VStack(alignment: .leading ,spacing: 8.adjustToScreenHeight) {
-                Text(record.title)
-                    .font(.pretendard(size: 16, weight: .regular))
-                    .foregroundStyle(.gray200)
-                
-                Text("\(record.hours)H \(record.minutes)M")
-                    .font(.pretendard(size: 16, weight: .semibold))
-                    .foregroundStyle(.gray100)
-                
-            }
-            .padding(.vertical, 18.adjustToScreenHeight)
-            .padding(.leading, 28.adjustToScreenWidth)
-            Spacer()
-        }
-        .frame(maxWidth: .infinity, maxHeight: 88.adjustToScreenHeight)
-        .background(.gray800)
-        .cornerRadius(4)
-        .overlay(
-            RoundedRectangle(cornerRadius: 4)
-                .stroke(.gray600, lineWidth: 1)
-        )
-    }
-}
-
 
 #Preview {
-    RecordView()
+    RecordView(store: Store(initialState: RecordFeature.State()) {
+        RecordFeature()
+    })
 }
