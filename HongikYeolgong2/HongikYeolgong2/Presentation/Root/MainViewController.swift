55b285be75337f41a99942cf6167624efb361e15
Choe-ju <juwon3415@gmail.com>
2025-01-16 16:52:52 +0900

    chore: info파일 위치명 수정

--- HongikYeolgong2/HongikYeolgong2/Presentation/Root/MainViewController.swift
+++ HongikYeolgong2/HongikYeolgong2/Presentation/Root/MainViewController.swift
@@ -6,11 +6,29 @@
 //
 
 import UIKit
+import Then
 
 class MainViewController: UIViewController {
+    
+    // 확인용 -> 다른 작업시 바로 삭제
+    private let testLabel = UILabel().then {
+        $0.text = "Test"
+        $0.textColor = .white
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupTestLable()
+    }
+    
+    private func setupTestLable() {
+        self.view.addSubview(testLabel)
+        
+        NSLayoutConstraint.activate([
+            testLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            testLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
 }
