f4a62f78cbe99dd3e162057e17e0ba40144f6400
Choe-ju <juwon3415@gmail.com>
2025-02-22 18:43:30 +0900

    feat: recoadView 기록 데이터 관련 view 리펙토링

--- HongikYeolgong2/Presentation/Record/RecordView.swift
+++ HongikYeolgong2/Presentation/Record/RecordView.swift
@@ -11,16 +11,19 @@ struct RecordView: View {
     @Environment(\.injected.interactors.studyTimeInteractor) var studyTimeInteractor
     @State private var studyTime: Loadable<StudyTime> = .notRequest
     
+    
+    @State private var selectedDate: Date = Date()
+    
     var body: some View {
         NetworkStateView(
             loadables: [AnyLoadable($studyTime)],
             retryAction: loadData
         ) {
             VStack(spacing: 13.adjustToScreenHeight) {
-                CaledarView()
-                Spacer()
                 // 데이터가 없을 경우 디폴트값으로 표현
-                StudyRecordView(studyTime: studyTime.value ?? StudyTime())
+                StudyRecordView(selectedDate: $selectedDate, studyTime: studyTime.value ?? StudyTime())
+                Spacer()
+                CaledarView()
             }
             .padding(.horizontal, 32.adjustToScreenWidth)
             .padding(.top, 32.adjustToScreenHeight)
