83f025a9e1bc1c107693d406a8377e61aa82d5f9
Choe-ju <juwon3415@gmail.com>
2025-03-23 20:44:20 +0900

    feat: 로그인 하는 경우 FCM 토큰 갱신하도록 수정

--- HongikYeolgong2/Domain/Interactors/UserDataInteractor+Migration.swift
+++ HongikYeolgong2/Domain/Interactors/UserDataInteractor+Migration.swift
@@ -107,6 +107,8 @@ final class UserDataMigrationInteractor: UserDataInteractor {
                     }
                     
                     KeyChainManager.addItem(key: .accessToken, value: loginResDto.accessToken)
+                    // FCM 토큰 업데이트 상태로 변경
+                    UserDefaults.standard.setValue(true, forKey: "isFCMTokenUpdated")
                 }
             )
             .store(in: cancleBag)
@@ -143,7 +145,7 @@ final class UserDataMigrationInteractor: UserDataInteractor {
     
     /// 로그아웃
     func logout() {
-        clearFCMTokenn()
+        clearFCMToken()
         appState[\.userSession] = .unauthenticated
         KeyChainManager.deleteItem(key: .accessToken)
     }
@@ -311,16 +313,13 @@ final class UserDataMigrationInteractor: UserDataInteractor {
     
     /// FCM Token 지우기
     /// 로그아웃 사용
-    private func clearFCMTokenn() {
+    private func clearFCMToken() {
         let userDefaults = UserDefaults.standard
         // 공백으로 업데이트
         authRepository
             .updateToken(fcmToken: "clear")
             .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { _ in }) { _ in
-                // 업데이트 상태 수정 -> ture : 다음 로그인 시 업데이트 적용 용도
-                userDefaults.setValue(true, forKey: "isFCMTokenUpdated")
-            }
+            .sink(receiveCompletion: { _ in }) { _ in }
             .store(in: cancleBag)
     }
 }
