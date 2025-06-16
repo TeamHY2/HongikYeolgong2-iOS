26ab771bb17a953085686f0598e57a45a590ed65
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-06-16 15:27:52 +0900

    feat: 포커스모드 UI 구현

---
+++ HongikYeolgong2/Presentation/Home/FocusMode/FocusModeView.swift
@@ -0,0 +1,71 @@
+//
+//  FocusModeView.swift
+//  HongikYeolgong2
+//
+//  Created by 권석기 on 6/16/25.
+//
+
+import SwiftUI
+
+struct FocusModeView: View {
+    let items = Array(1...69)
+    let columns = Array(repeating: GridItem(.flexible(), spacing: 12), count: 4)
+    var body: some View {
+        VStack(spacing: 0) {
+            VStack(spacing: 0) {
+                StudyPeriodView(startTime: .now, endTime: .now)
+                StudyTimerView(totalTime: .seconds(0), remainingTime: .seconds(0), color: .white)
+                
+                HStack(spacing: 12.adjustToScreenWidth) {
+                    BaseButton(
+                        title: "열람실 이용 연장",
+                        backgroundColor: .blue100,
+                        radius: 4,
+                        action: {}
+                    )
+                    BaseButton(
+                        title: "열람실 이용 종료",
+                        backgroundColor: .gray600,
+                        radius: 4,
+                        action: {  }
+                    )
+                }
+                .padding(.top, 28)
+            }
+            .padding(.horizontal, 32)
+            .padding(.top, 28)
+            VStack(spacing: 4) {
+                Text("전체")                    
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.suite(size: 16, weight: .regular), lineHeight: 26.adjustToScreenHeight)
+                    .foregroundStyle(.gray100)
+                HStack {
+                    Text("12명")
+                        .font(.suite(size: 14, weight: .regular), lineHeight: 20.adjustToScreenHeight)
+                        .foregroundStyle(.blue100)
+                    Text("공부중")
+                        .font(.suite(size: 14, weight: .regular), lineHeight: 20.adjustToScreenHeight)
+                        .foregroundStyle(.gray300)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding(.horizontal, 32)
+           
+            .padding(.top, 36)
+            .padding(.bottom, 20)
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 12) {
+                   
+                    ForEach(items, id: \.self) { item in
+                        FocusCell()
+                    }
+                }
+                .padding(.horizontal, 32)
+            }
+        }
+    }
+}
+
+//#Preview {
+//    FocusModeView()
+//}
