47e989f178626559383c86404c5359cb26c9d260
Choe-ju <juwon3415@gmail.com>
2025-06-21 22:43:29 +0900

    feat: 포커스모드 관련 구조로 변경된 데이터 관련 DTO 추가

---
+++ HongikYeolgong2/Data/DTO/StudySession/StartStudyRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  StartStudyRequestDTO.swift
+//  HongikYeolgong2
+//
+//  Created by 최주원 on 6/21/25.
+//
+
+import Foundation
+
+struct StartStudyRequestDTO: Codable {
+    let startTime: String
+}
