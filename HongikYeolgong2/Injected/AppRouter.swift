a4a747c2ee695cbc21ae5f6b6ad38ba4807259b8
Choe-ju <juwon3415@gmail.com>
2025-11-20 23:08:16 +0900

    feat: 뒤로가기 스와이프 액션 추가

--- HongikYeolgong2/Injected/AppRouter.swift
+++ HongikYeolgong2/Injected/AppRouter.swift
@@ -77,3 +77,15 @@ extension View {
         }
     }
 }
+
+// toolbar -> Hidden 상태 뒤로가기 제스쳐 활성화
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
