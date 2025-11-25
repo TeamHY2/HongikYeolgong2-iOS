2d689d0f9eeb53c0d1308a6ff17a317a42a9a191
Choe-ju <juwon3415@gmail.com>
2025-11-25 18:22:56 +0900

    feat: 친구 랭킹 리스트 관련 DTO, Entitie, Mapper 추가

---
+++ HongikYeolgong2/Domain/Entities/Friend/FriendStudyTime.swift
@@ -0,0 +1,37 @@
+//
+//  FriendStudyTime.swift
+//  HongikYeolgong2
+//
+//  Created by 최주원 on 11/25/25.
+//
+
+import Foundation
+
+struct FriendStudyTime: Hashable {
+    let userId: Int
+    let friendId: Int
+    let friendNickname: String
+    let studyTime: Date
+    
+    // "hhH mmM" 형태 반환
+    var studyTimeString: String {
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: studyTime)
+        let minute = calendar.component(.minute, from: studyTime)
+
+        return "\(hour)H \(minute)M"
+    }
+    
+    init(userId: Int, friendId: Int, friendNickname: String, studyTime: String) {
+        // Date형태 변환
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        let studyTime = formatter.date(from: studyTime)!
+        
+        self.userId = userId
+        self.friendId = friendId
+        self.friendNickname = friendNickname
+        self.studyTime = studyTime
+    }
+}
