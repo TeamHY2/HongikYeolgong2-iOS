60b838c5f5a2a30e8342f0338faa3511cbf551ae
Choe-ju <juwon3415@gmail.com>
2026-05-05 22:27:06 +0900

    fix: 열람실 이용 시작시 포커스모드 자동 실행 부분 제거

--- HongikYeolgong2/Presentation/Home/HomeView.swift
+++ HongikYeolgong2/Presentation/Home/HomeView.swift
@@ -148,10 +148,10 @@ extension HomeView {
         studySessionInteractor.startStudy()
         weeklyStudyInteractor.addStarCount(studyRecords: $studyRecords)
         // 포커스모드 실행
-        Task {
-            try await Task.sleep(nanoseconds: 500_000_000)
-            activateFocusMode()
-        }
+//        Task {
+//            try await Task.sleep(nanoseconds: 500_000_000)
+//            activateFocusMode()
+//        }
         Amplitude.instance.track(eventType: "StudyStartButton")
     }
     
